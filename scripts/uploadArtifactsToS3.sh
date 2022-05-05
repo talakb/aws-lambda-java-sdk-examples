bac50382d0670aaf38e00fb431d73b30f2494576
Talak B <talakb@users.noreply.github.com>
2022-05-04 19:01:35 -0500

    Update uploadArtifactsToS3.sh

--- scripts/uploadArtifactsToS3.sh
+++ scripts/uploadArtifactsToS3.sh
@@ -2,6 +2,9 @@
 #TODO - parametrize  below variables to use the script for other examples too 
 #./uploadArtifactsToS3.sh aws-java-sdk-examples-source-code lambda lambda-1.0-SNAPSHOT.jar
 
-BUCKET_NAME=aws-java-sdk-examples-source-code
-FOLDER_NAME=lambda
-aws s3 cp ../target/lambda-1.0-SNAPSHOT.jar s3://$BUCKET_NAME/$FOLDER_NAME
+BUCKET_NAME=$1
+FOLDER_NAME=$2
+JAR_FILE_NAME=$3
+
+
+aws s3 cp ../target/$JAR_FILE_NAME s3://$BUCKET_NAME/$FOLDER_NAME
