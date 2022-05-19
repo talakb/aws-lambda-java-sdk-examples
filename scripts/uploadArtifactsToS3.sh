10ff40c51d9e32f5c07571ba919052f03c2027f4
Talak Belay <talakbelay@gmail.com>
2022-05-19 14:12:31 -0500

    Added Lambda Authorizer example based on AWS code/blueprint logic.

--- scripts/uploadArtifactsToS3.sh
+++ scripts/uploadArtifactsToS3.sh
@@ -2,9 +2,9 @@
 #TODO - parametrize  below variables to use the script for other examples too 
 #./uploadArtifactsToS3.sh aws-java-sdk-examples-source-code lambda lambda-1.0-SNAPSHOT.jar
 
-BUCKET_NAME=$1
-FOLDER_NAME=$2
-JAR_FILE_NAME=$3
+BUCKET_NAME=aws-java-sdk-examples-source-code
+FOLDER_NAME=lambda
+JAR_FILE_NAME=lambda-1.0-SNAPSHOT.jar
 
 
-aws s3 cp ../target/$JAR_FILE_NAME s3://$BUCKET_NAME/$FOLDER_NAME
+aws s3 cp ../target/$JAR_FILE_NAME s3://$BUCKET_NAME/$FOLDER_NAME/$JAR_FILE_NAME
